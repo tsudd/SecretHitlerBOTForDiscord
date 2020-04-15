7c2d52282aadbf115de0d49ffb40c78a3a02edb5
Tsudd <alex.kr1stehko@gmail.com>
2020-04-15 22:27:51 +0300

    Starting

---
+++ Bot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
